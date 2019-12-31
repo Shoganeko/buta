2ff6409cc0bfa86eaeff0c7c35c01813f47ba7e2
sho <blaziyt@gmail.com>
2019-12-31 00:34:02 -0600

    Added some documentation

--- src/main/java/dev/shog/buta/handle/audio/DiscordAudioProvider.kt
+++ src/main/java/dev/shog/buta/handle/audio/DiscordAudioProvider.kt
@@ -6,13 +6,22 @@ import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame
 import discord4j.voice.AudioProvider
 import java.nio.ByteBuffer
 
+/**
+ * Provides audio to discord.
+ */
 class DiscordAudioProvider(private val player: AudioPlayer) : AudioProvider(ByteBuffer.allocate(StandardAudioDataFormats.DISCORD_OPUS.maximumChunkSize())) {
+    /**
+     * I don't even know
+     */
     private val frame = MutableAudioFrame()
 
     init {
         frame.setBuffer(buffer)
     }
 
+    /**
+     * Try to provide to Discord.
+     */
     override fun provide(): Boolean {
         return try {
             val didProvide = player.provide(frame)
--- src/main/java/dev/shog/buta/handle/audio/GuildMusicManager.kt
+++ src/main/java/dev/shog/buta/handle/audio/GuildMusicManager.kt
@@ -8,13 +8,39 @@ import discord4j.core.`object`.entity.TextChannel
 import discord4j.voice.VoiceConnection
 import java.lang.Exception
 
+/**
+ * A guild music manager.
+ * This manages a guild's music operations.
+ */
 class GuildMusicManager(manager: AudioPlayerManager) {
+    /**
+     * The actual player.
+     */
     val player: AudioPlayer = manager.createPlayer()
+
+    /**
+     * The track scheduler. This allows for a queue of songs.
+     */
     val scheduler: TrackScheduler = TrackScheduler(player)
+
+    /**
+     * This provides audio to Discord.
+     */
     val provider: DiscordAudioProvider = DiscordAudioProvider(player)
+
+    /**
+     * The voice connection to the channel. This allows for later disconnecting.
+     */
     var connection: VoiceConnection? = null
+
+    /**
+     * The channel where music playing was requested.
+     */
     var requestChannel: MessageChannel? = null
 
+    /**
+     * Stop playing, clear tracks, and disconnect.
+     */
     fun stop() {
         scheduler.clearTracks()
         player.stopTrack()
